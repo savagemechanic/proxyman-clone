6778598d01a1b2cef14410a27f92d8d32179492a
Savage Mechanic <savagemechanic@github.com>
2026-09-08 19:27:43 +0100

    feat: add macOS clipboard helper

---
+++ macos/Sources/ProxymanCloneApp/Clipboard.swift
@@ -0,0 +1,10 @@
+import AppKit
+
+@MainActor
+enum Clipboard {
+    static func copy(_ text: String) {
+        let pasteboard = NSPasteboard.general
+        pasteboard.clearContents()
+        pasteboard.setString(text, forType: .string)
+    }
+}
